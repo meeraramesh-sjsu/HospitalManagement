939b1d8029c5487844a98d6fa2136d27f6d7b0e9
Meera Ramesh <meekar67@gmail.com>
2016-02-07 23:01:30 -0800

    PHP pages for a common page for all receptionist pages, view the bills of inpatient and outpatient for receptionist, and bill page for Patient was created.
    PHP pages to make a connection to operational and analytical database was also added.

---
+++ src/connection.php
@@ -0,0 +1,4 @@
+<?php
+ $con = new PDO("mysql:host=localhost;dbname=hospital","root", "y");
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
---
+++ src/connectionAnalytics.php
@@ -0,0 +1,4 @@
+<?php
+ $con = new PDO("mysql:host=localhost;dbname=hospitalanalytics","root", "y");
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
